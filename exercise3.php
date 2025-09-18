59104c1fb2a30bf275b3a07263759620c83ce15d
AlexBeltran31 <alexbeltranrt@gmail.com>
2025-09-18 10:55:48 +0200

    Exercise 1 done

---
+++ exercise3.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1 = 12;
+$num2 = 24;
+$num3 = 31;
+$num4 = 8;
+$num5 = 67;
+
+$array = [$num1, $num2, $num3, $num4, $num5];
+echo $array [0] . "\n";
+echo $array [1] . "\n";
+echo $array [2] . "\n";
+echo $array [3] . "\n";
+echo $array [4] . "\n";
+
+print_r($array);
+
+// dos formas de mostrar una array por pantalla
