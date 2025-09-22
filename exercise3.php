7bc1b3fb57dd5f611ac255c4dd11fcdc73be94f1
AlexBeltran31 <alexbeltranrt@gmail.com>
2025-09-22 22:35:19 +0200

    Exercise 3 done

--- exercise3.php
+++ exercise3.php
@@ -23,3 +23,23 @@ echo "Size of the array: " . count($colors) . "\n";
 unset($colors[3]);
 echo "New size of the array: " . count($colors) . "\n";
 print_r($colors);
+
+// ex 3
+function containChar(array $words, string $char): bool {
+    if (strlen($char) !== 1) {
+        return false;
+    }
+
+    foreach ($words as $word) {
+        if (stripos($word, $char) === false) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+$words = ["hola", "php", "html"];
+echo containChar($words, "h") ? "true\n" : "false\n";
+echo containChar($words, "l") ? "true\n" : "false\n";
+echo containChar($words, "r") ? "true\n" : "false\n";
