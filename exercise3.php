009e1c78b9d76b84772e4b65406dd851b51ae524
AlexBeltran31 <alexbeltranrt@gmail.com>
2025-09-28 14:35:48 +0200

    Exercise 3 explained

--- exercise3.php
+++ exercise3.php
@@ -26,17 +26,25 @@ print_r($colors);
 
 // ex 3
 function containChar(array $words, string $char): bool {
-    if (strlen($char) !== 1) {
+    //creamos una función que recibe una array y un "character" que queda definido como string.
+    //Devuelve verdadero o falso
+    if (strlen($char) !== 1) { //strlen calcula el tamaño de $char
+        //si no es idéntico a 1 (!==) devuelve falso,
+        //validando que sea un único (1) caracter
         return false;
     }
 
-    foreach ($words as $word) {
+    foreach ($words as $word) { //bucle foreach para recorrer cada palabra del array
         if (stripos($word, $char) === false) {
+            //stripos busca $char dentro de cada $word de la array.
+            //Si pongo únicamente dos = me devolvía siempre false si el $char
+            //no era la primera letra de cada palabra. Con los tres = recorre la palabra entera
+            //No entendí muy bien la diferencia entre equal (==) y identical (===)
             return false;
         }
     }
 
-    return true;
+    return true; //devuelve true si todas las palabras contienen el $char
 }
 
 $words = ["hola", "php", "html"];
